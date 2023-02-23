b7f954445cfd2f80bd175c4e5f952a2222d2eef2
Sean Kim <sgkim@usc.edu>
2023-02-23 00:42:41 +0000

    finish

--- logicsim/circuit.h
+++ logicsim/circuit.h
@@ -1,68 +1,29 @@
-#ifndef STACK_H
-#define STACK_H
-
+#ifndef CIRCUIT_H
+#define CIRCUIT_H
 #include <vector>
-#include <stdexcept>
 
-// Use inheritance from std::vector (choose public/private) as appropriate
-template <typename T>
-class Stack : private std::vector<T>
+#include "../heap.h"
+#include "event.h"
+#include "gate.h"
+
+class Circuit 
 {
-public:
-    Stack();
-    ~Stack();
-    bool empty() const;
-    size_t size() const;
-    void push(const T& item);
-    void pop();  // throws std::underflow_error if empty
-    const T& top() const; // throws std::underflow_error if empty
-    // Add other members only if necessary
+	public:
+		Circuit();
+		~Circuit();
+		void test();
+		bool advance(std::ostream&);
+		void run(std::ostream&);
+    bool parse(const char*);
+    void startUml(std::ostream&);
+    void endUml(std::ostream&);
+		
+	private:
+		uint64_t m_current_time;
+    std::vector<Gate*> m_gates;
+    std::vector<Wire*> m_wires;
+		// we need to add the m_pq data member. It should be a min-heap of Event*;
+		Heap<Event*,EventLess> m_pq;
 };
-
-template <typename T>
-Stack<T>::Stack() : std::vector<T>(){
-
-}
-
-template <typename T>
-Stack<T>::~Stack() {
-
-}
-
-template <typename T>
-bool Stack<T>::empty() const{
-    if(std::vector<T>::empty()){
-        return true;
-    }
-    return false;
-}
-
-template <typename T>
-size_t Stack<T>::size() const{
-    return std::vector<T>::size();
-}
-
-template <typename T>
-void Stack<T>::push(const T& item){
-    std::vector<T>::push_back(item);
-}
-
-template <typename T>
-void Stack<T>::pop() {
-    if(empty()){
-        throw std::underflow_error("No element to pop");
-    }
-    std::vector<T>::pop_back();
-
-}
-
-template <typename T>
-const T& Stack<T>::top() const {
-    if(empty()){
-        throw std::underflow_error("No elements in list");
-    }
-    return std::vector<T>::back();
-}
-
 
 #endif
