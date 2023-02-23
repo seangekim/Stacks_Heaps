7f16305569b8d2c798be74d2daeb5ad74389447b
Sean Kim <sgkim@usc.edu>
2023-02-23 05:09:52 +0000

    final

--- llrec.cpp
+++ llrec.cpp
@@ -1,5 +1,4 @@
 #include "llrec.h"
-#include "iostream"
 
 //*********************************************
 // Provide your implementation of llpivot below
@@ -15,7 +14,6 @@ void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
   }
 //check if current node val is less than or equal to pivot
   if(head->val <= pivot){
-    // std::cout << "less than pivot " << head->val << std::endl;
   // if node is less or equal, check if head
       smaller = head;
       head = head->next;
